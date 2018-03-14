c05b0bff82e823c5910ec366b64ea3b03e3d7179
akimov-dmitry <34105756+akimov-dmitry@users.noreply.github.com>
2018-03-14 14:33:30 +0300

    Add files via upload

---
+++ screenshot_bot.py
@@ -0,0 +1,38 @@
+import telegram
+from PIL import Image, ImageGrab
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import re
+import time
+token = '' #Input your token here
+
+
+def start(bot, update):
+    bot.send_message(update.message.chat_id, text="Добро пожаловать!")
+    bot.send_message(update.message.chat_id, text="Введите код") 
+def answer(bot, update):
+    if update.message.text=="":   # Print here your pin
+        bot.send_message(update.message.chat_id, text="Удачно!")
+        img = ImageGrab.grab()
+        img.save("", "JPEG")   #Print route to folder. It must be same where screenshot_bot.py is located
+        bot.sendPhoto(chat_id=update.message.chat_id, photo=open('temp_screen.jpg', 'rb'))
+        bot.send_message(update.message.chat_id, text="Температура")
+    else:
+        bot.send_message(update.message.chat_id, text="Ошибка!")
+
+
+
+    
+def main():
+
+    updater = Updater(token)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    #dp.add_handler (CallbackQueryHandler(button))
+    dp.add_handler(MessageHandler([Filters.text], answer))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
